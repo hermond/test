5be58e069447fb86ad3c8f92cb333f2b1b01ec21
hermond <axelaff@gmail.com>
2013-09-04 15:54:50 +0200

    first commit

---
+++ FormView.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Hello World
+ * Date: 2013-09-04
+ * Time: 15:01
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace view;
+
+
+class FormView {
+
+public function getPage($title, )
+{
+
+
+}
+}
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Hello World
+ * Date: 2013-09-04
+ * Time: 14:59
+ * To change this template use File | Settings | File Templates.
+ */
+
+$formView = new \view\FormView();
+$formview-->
+
